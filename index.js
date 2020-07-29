3a2709274fe60c5341d36ac594d2ef485c9c69c0
irenexu <kiki19962007bc@gmail.com>
2020-07-29 12:35:04 +0800

    server test

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require ('express');
+const app =express();
+
+const port = 4000;
+
+app.listen(port, () => {
+    console.log("server is running on port " + port)
+});
+app.get('/', (request, response) => {
+    response.send('server test')
+})
+
