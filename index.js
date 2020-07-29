8161e4865bac05a50dce84a56936904297aa5630
irenexu <kiki19962007bc@gmail.com>
2020-07-29 14:19:09 +0800

    connect db

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const express = require ('express');
-const app =express();
-
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const { response } = require('express');
 const port = 4000;
+const keys = require('./config/keys').mongoUri
+
+mongoose.connect(keys.mongoUri, { useNewUrlParser: true })
+    .then(() => {
+        console('server connect db')
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 
 app.listen(port, () => {
     console.log("server is running on port " + port)
