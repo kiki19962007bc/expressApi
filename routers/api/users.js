f09bb498ac56ae12b68692c8b7153b81d60805b8
irenexu <kiki19962007bc@gmail.com>
2020-07-29 17:31:06 +0800

    login

--- routers/api/users.js
+++ routers/api/users.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const User = require('../../models/User');
+
 const router = express.Router();
 
 router.post('/register', (request, response) => {
@@ -15,12 +19,43 @@ router.post('/register', (request, response) => {
                     email: request.body.email,
                     password: request.body.password,
                 });
-                newUser.save()
-                    .then(user => response.json(user))
-                    .catch(error => response.json({ status: 'error', data: error }));
+                bcrypt.genSalt(10, (error, salt) => {
+                    bcrypt.hash(newUser.password, salt, (error, hash) => {
+                        if (error) {
+                            throw error;
+                        }
+                        newUser.password = hash;
+                        newUser.save()
+                            .then(user => response.json(user))
+                            .catch(error => response.json({ status: 'error', data: error }));
+                    })
+                })
             }
         })
 })
+
+router.post('/login',(request,response) => {
+    const email = request.body.email;
+    const password = request.body.password;
+    User.findOne({ email: request.body.email })
+    .then(user => {
+        if(user){
+            bcrypt.compare(password, user.password)
+            .then(result =>{
+                if(result){
+                    return response.json({ status: 'success', message: '登入成功' });
+                }
+                else{
+                    return response.json({ status: 'error', message: '密碼錯誤' });
+                }
+            })
+        }       
+        else{
+            return response.json({ status: 'error', message: '無此登入email' });
+        }
+    })
+})
+
 
 router.get('/', (request, response) => {
     User.find({}, (error, users) => {
